ce93499663db528956aa306322604a0ed7e7c4ea
charliehawley <charlie.e.hawley@gmail.com>
2022-06-26 16:47:40 +0000

    Import artwork and initialise the products app.

--- full_power/urls.py
+++ full_power/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('favicon.ico', 
          RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
