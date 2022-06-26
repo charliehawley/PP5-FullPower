63bc077226f83612ddddbaaa7a698a88ddee9c85
charliehawley <charlie.e.hawley@gmail.com>
2022-06-26 18:11:45 +0000

    Set up music and products models.

---
+++ music/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Band(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Music(models.Model):
+    name = models.CharField(max_length=254, null=True, blank=True)
+    band = models.ForeignKey('Band', null=True, blank=True, on_delete=models.SET_NULL)
+    embed_code = models.CharField(max_length=1024, null=True, blank=True)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.name
