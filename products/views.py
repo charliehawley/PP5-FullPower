4653b45efa9f21aee5cac7251f90ff2962cafab1
charliehawley <charlie.e.hawley@gmail.com>
2022-07-05 09:11:49 +0000

    Create product search view.

--- products/views.py
+++ products/views.py
@@ -8,9 +8,21 @@ def all_products(request):
     """ Returns products page """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You haven't entered any search terms!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search term': query
     }
 
     return render(request, 'products/products.html', context)
