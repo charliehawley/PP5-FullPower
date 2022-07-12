69708dedff955f7db05ae70238d0cab2e247b4d8
charliehawley <charlie.e.hawley@gmail.com>
2022-07-12 10:28:40 +0000

    Set up heroku deployment, part 2.

--- full_power/settings.py
+++ full_power/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b@+@eg!qovu9eqi-wklefci&180m(!^otf@8^!l_66ln#=h=_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['full-power-records.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,12 +107,17 @@ WSGI_APPLICATION = 'full_power.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
